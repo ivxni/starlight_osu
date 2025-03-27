453f0fc34b6acf2e98c20da794b8104518ca4d57
ivxni <angelmxycry@outlook.com>
2025-03-28 00:15:42 +0100

    Commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#include <winuser.h>
+#include <windows.h>
+#include <tlhelp32.h>
 
 using namespace std;
 
 int main()
 {
+    const HWND hwnd = FindWindowA(nullptr, "osu!");
+    DWORD pid = 0;
+    GetWindowThreadProcessId(hwnd, &pid);
+
+    uintptr_t getModuleBase(DWORD procId, const wchar_t* modName)
+    {
+        uintptr_t modBase = 0;
+        constexpr DWORD procId = 0;
+        HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+    }
+
 
     return 0;
 }
